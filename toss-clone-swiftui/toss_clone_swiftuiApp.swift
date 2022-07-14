1da642faeac16237d46f6387256a6ea582b5b83d
Collie <imtophun@gmail.com>
2022-07-14 09:46:52 +0900

    feat: 프로젝트 생성

---
+++ toss-clone-swiftui/toss_clone_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  toss_clone_swiftuiApp.swift
+//  toss-clone-swiftui
+//
+//  Created by Collie on 2022/07/14.
+//
+
+import SwiftUI
+
+@main
+struct toss_clone_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
